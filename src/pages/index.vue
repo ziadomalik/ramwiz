bf636b8b108aeb63e24f161fba39e4f46b8e8dbb
Ziad Malik <me@ziadmalik.com>
2026-01-06 02:11:57 +0100

    chore: set `cursor-pointer` for the filepicker

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 
 const value = ref<File | null>(null);
+
+const ui = {
+  fileTrailingButton: "cursor-pointer",
+};
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center h-screen">
     <div class="div w-full max-w-sm">
-      <UFileUpload v-model="value" class="w-96 min-h-48" layout="list" />
-      <UButton block class="mt-4" icon="i-lucide-upload" :disabled="!value" size="lg">Load Trace</UButton>
+      <UFileUpload
+        v-model="value"
+        class="w-96 min-h-48"
+        layout="list"
+        :ui="ui"
+      />
+      <UButton
+        block
+        class="mt-4 cursor-pointer"
+        icon="i-lucide-upload"
+        :disabled="!value"
+        size="lg"
+      >
+        Load Trace
+      </UButton>
     </div>
   </div>
 </template>
