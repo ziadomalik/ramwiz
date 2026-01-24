1ccb41f0360c80e48c0b45d31b7078f781807ed4
Ziad Malik <me@ziadmalik.com>
2026-01-24 17:06:00 +0100

    chore: make infobar more compact

--- app/components/InfoBar.vue
+++ app/components/InfoBar.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="flex justify-end gap-4 bg-neutral-800 text-neutral-200 border-t border-neutral-700 px-2 py-1 text-xs font-mono w-full">
     <div class="flex gap-4">
-      <div class="flex flex-col gap-1.5 w-64">
+      <div class="flex flex-col gap-1.5">
         <div class="flex items-center justify-between gap-1.5">
           <div class="flex gap-1.5">
             <span class="text-neutral-400">Events Loaded: </span>
-            <span class="font-bold">{{ formattedEvents }}</span>
+            <div class="flex gap-1.5 w-28 justify-end">
+              <span class="font-bold">{{ formattedEvents }}</span>
+              <span class="font-bold text-neutral-400">({{ eventsLoadedPercentage }}%)</span>
+            </div>
           </div>
-          <div class="flex gap-1.5">
+          <div class="flex gap-1.5 border-l border-neutral-700 pl-2 min-w-12">
             <span class="text-neutral-400">FPS:</span>
-            <span class="font-bold">{{ fps }}</span>
+            <span class="font-bold">{{ formattedFPS }}</span>
           </div>
-        </div>
-        <div class="flex items-center gap-1.5">
-          <UProgress :model-value="progress" :max="100" size="sm" color="success" />
-          <span class="font-bold text-neutral-400">({{ eventsLoadedPercentage }}%)</span>
         </div>
       </div>
     </div>
@@ -35,12 +34,13 @@ const props = withDefaults(defineProps<{
 const formatter = new Intl.NumberFormat('en-US');
 
 const eventsLoadedPercentage = computed(() => {
-  const pct = Math.round((props.eventCount / props.totalEvents) * 100);
-  return pct < 10 ? `0${pct}` : `${pct}`;
+  const pct = Math.round((props.eventCount / props.totalEvents) * 100) || 0;
+  return formatter.format(pct).padStart(3, '0');
 });
 
-const progress = computed(() => {
-  return Math.round((props.eventCount / props.totalEvents) * 100) || 0;
+const formattedFPS = computed(() => {
+  const padding = props.fps < 10 ? 2 : 0;
+  return formatter.format(props.fps).padStart(padding, '0');
 });
 
 const formattedEvents = computed(() => {
